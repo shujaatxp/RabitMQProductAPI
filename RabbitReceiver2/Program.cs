4ea43ead84a095f44b68e800970cb0c738365d6c
User <ali.shujjat@gmail.com>
2023-06-04 18:59:15 +0400

    commit

---
+++ RabbitReceiver2/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+// Here we specify the Rabbit MQ Server. we use rabbitmq docker image and use it
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver2 App";
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangename = "demoexchange";
+string routingkey = "demo-routing-key";
+string queuename = "demoqueue";
+
+channel.ExchangeDeclare(exchangename, ExchangeType.Direct);
+channel.QueueDeclare(queuename, false, false, false, null);
+channel.QueueBind(queuename, exchangename, routingkey, null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) => {
+
+    Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    //send message to database at this level
+    Console.WriteLine($"Message received by receiver2: {message}");
+
+    channel.BasicAck(args.DeliveryTag, false);
+
+};
+
+string consumertag = channel.BasicConsume(queuename, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumertag);
+
+channel.Close();
+connection.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+//Here we specify the Rabbit MQ Server. we use rabbitmq docker image and use it
+ConnectionFactory factory = new ();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Send App";
+
+IConnection connection = factory.CreateConnection ();
+IModel channel = connection.CreateModel();
+
+string exchangename = "demoexchange";
+string routingkey = "demo-routing-key";
+string queuename = "demoqueue";
+
+channel.ExchangeDeclare(exchangename, ExchangeType.Direct);
+channel.QueueDeclare(queuename, false, false, false, null);
+channel.QueueBind(queuename, exchangename, routingkey, null);
+
+for (int i = 0; i <= 50; i++)
+{
+    string message = $"hello youtube {i}";
+    Console.WriteLine($"Message sent to Receiver: {message}");
+
+    var json = JsonConvert.SerializeObject(message);
+    var messagebodybytes = Encoding.UTF8.GetBytes(json);
+   
+    channel.BasicPublish(exchangename, routingkey, null, messagebodybytes);
+
+    Thread.Sleep(1000);
+}
+
+
+channel.Close ();
+connection.Close ();
